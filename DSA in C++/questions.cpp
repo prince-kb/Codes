15d98727c1659c5ea9ed367880c1b04706dc5bc0
Prince K <princejaj67@gmail.com>
2022-10-04 19:45:08 +0530

    Testing

--- DSA in C++/questions.cpp
+++ DSA in C++/questions.cpp
@@ -10,6 +10,26 @@ for(int i=0;i<n;i++)
 cout<<arr[i]<<" ";
 cout<<endl;
 }
+void insertion_sort(int arr[],int n){
+    int i=0,j;
+    while(i<n){
+        int temp=arr[j];
+        while(j>=0){
+            if(arr[j]<temp){
+                arr[j+1]=arr[j];
+            }
+            else {
+                j--;
+                break;
+            }
+        }
+        printArray(arr,n);
+        arr[j]=temp;
+        i++;
+    }
+
+}
+/* 
 int main(){
 int a,b,arr1[50],arr2[50],arr[100];
 cout<< "Enter number of items of array 1" << endl;
@@ -38,4 +58,12 @@ k++;
 }
 cout<<"Merged array is -"<<endl;
 printArray(arr,k);
+} */
+int main(){
+    int arr[50],n;
+    cout<<"Enter number of elements of array: ";
+    cin>>n;
+    ins(arr,n);
+    insertion_sort(arr,n);
+    printArray(arr,n);
 }

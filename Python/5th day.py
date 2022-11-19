bc1be241ccc2eefb8b89ee7ea877d16843cb58d4
Prince K <princejaj67@gmail.com>
2022-11-19 15:05:00 +0530

    All

---
+++ Python/5th day.py
@@ -0,0 +1,22 @@
+# 1>max and min of list without using 
+""" 
+l=[3,6,2,5,9,0]
+max=l[0]
+min=l[0]
+for i in range(len(l)):
+    if l[i]>max:
+        max=l[i]
+    if l[i]<min:
+        min=l[i]
+print(max,min)
+ """
+# 2>Multiply two matrices as nested lists
+l1=[[1,2],[3,4]]
+l2=[[5,6],[7,8]]
+l3=[[],[]]
+for i in range(0,2,1):
+    for j in range(0,2,1):
+        print(l1[i][j])
+    #     # for k in range(0,2,1):
+    # print(" ")
+

c7845924bb0262c3088202641810973dda29a37e
Prince K <princejaj67@gmail.com>
2022-11-29 20:38:16 +0530

    Strings

--- DSA in C++/Rough/12-strings.cpp
+++ DSA in C++/Rough/12-strings.cpp
@@ -1,3 +1,4 @@
+/* //Palindrome checker
 #include<iostream>
 using namespace std;
 int getLength(char a[]){
@@ -23,7 +24,7 @@ auto smallarize(char a[],int len){
         a[i]=a[i]-'A'+'a';
         i+=1;
     }
-}
+}  
 bool palindrome_for_mixed(char a[],int len){
     int s=0,e=len-1;
     while(s<=e){
@@ -49,4 +50,50 @@ int len=getLength(a);
 smallarize(a,len);
 cout<<a<<endl;
 cout<<palindrome_for_mixed(a,len);
+}
+ */
+
+//Reverse a sentence
+#include<iostream>
+using namespace std;
+void reverse_string(string &b){
+    for(int i=0;i<b.length()/2;i++)
+    swap(b[i],b[b.length()-1-i]);
+
+}
+void push(string &a,string c){
+    for(int i=0;i<c.length();i++)
+    a.push_back(c[i]);
+}
+string reverse(string &a,string &c){
+    int i=a.length()-1;
+    while(i>=0){
+        string b;
+        while(1){
+            if(a[i]==' ' || i<0){
+                i--;
+            break;
+            }
+            else b.push_back(a[i]);
+            i-=1;
+        }
+        reverse_string(b);
+        push(c,b);
+        if(i!=0)
+        c.push_back(' ');
+        // cout<<a<<endl;
+}
+return c;
+}
+
+int main(){
+string a,c;
+// cout<< "Enter the sentence: ";
+// cin>>a;
+a="This is elephant";
+cout<<a<<endl;
+// reverse_string(a);
+// cout<<a<<endl;
+cout<<"Reversed string is: "<<endl;
+cout<<reverse(a,c);
 }

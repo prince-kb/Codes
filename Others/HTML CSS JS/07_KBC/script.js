388807d0eb5ee5a2223b4c94917a229ce7179700
Prince K <princejaj67@gmail.com>
2022-12-19 19:25:02 +0530

    quiz app

--- Others/HTML CSS JS/07_KBC/script.js
+++ Others/HTML CSS JS/07_KBC/script.js
@@ -20,35 +20,111 @@ const opt2=document.getElementById("opt2");
 const opt3=document.getElementById("opt3");
 const opt4=document.getElementById("opt4");
 const money=document.getElementById("money");
-const next=document.getElementById("next");
-let i=0;
-function display1(){
-    if(1){
-        question.innerHTML=q[i];{
-            let j=0;
-        opt1.innerHTML=opt[(i*4) + j++];
-        opt2.innerHTML=opt[(i*4) + j++];
-        opt3.innerHTML=opt[(i*4) + j++];
-        opt4.innerHTML=opt[(i*4) + j++];
-        if(j==5){
-            j=0;
-        }
-        }
+const next=document.getElementById("id");
+next.style.visibility="visible";
+next.addEventListener("click",display);
+let i=-1;
+function won(x){
+    opt1.style.diplay="none";
+    opt2.style.diplay="none";
+    opt3.style.diplay="none";
+    opt4.style.diplay="none";
+    x.style.display="hidden";
+}
+
+function lose(x){
+    let y=ans[i];
+    opt1.style.diplay="none";
+    opt2.style.diplay="none";
+    opt3.style.diplay="none";
+    opt4.style.diplay="none";
+    x.style.display="hidden";
+    if(opt1.innerHTML==y){
+        opt1.style.display="hidden";
+        opt1.style.background="blue";
     }
+    if(opt2.innerHTML==y){
+        opt1.style.display="hidden";
+        opt2.style.background="blue";
+    }
+    if(opt3.innerHTML==y){
+        opt1.style.display="hidden";
+        opt3.style.background="blue";
+    }
+    if(opt4.innerHTML==y){
+        opt1.style.display="hidden";
+        opt4.style.background="blue";
+    }
+    x.style.background="red";
+
+}
+function blink(x){
+    let a=setInterval(() => {
+        let p=0;
+        if(x.style.background=="red"){
+            p++;
+            x.style.background=="green"
+        }
+        else{
+            x.style.background=="red";
+            p++;
+        }
+        if(p==7){
+            clearInterval(a);
+        }
+    }, 1000);
 }
 function display(){
-            question.innerHTML=q[i];
-            {
-                let j=0;
-            opt1.innerHTML=opt[(i*4) + j++];
-            opt2.innerHTML=opt[(i*4) + j++];
-            opt3.innerHTML=opt[(i*4) + j++];
-            opt4.innerHTML=opt[(i*4) + j++];
-            if(j==5){
-                j=0;
-            }
-        }}
+    i++;
+    if(i==5){
+        i=0;
+    }
+    let j=0;
+    question.innerHTML=q[i];
+    opt1.innerHTML=opt[i*4 + j++];
+    opt2.innerHTML=opt[i*4 + j++];
+    opt3.innerHTML=opt[i*4 + j++];
+    opt4.innerHTML=opt[i*4 + j++];
+    if(j==5)
+    j=0;
+    choose();
+}
 
 function choose(){
-    
+    opt1.addEventListener("click",function(){
+        if(opt1.innerHTML==ans[i]){
+            next.style.visibility="visible";
+            opt1.style.background="green";
+            opt2.style.background="red"
+            opt3.style.background="red"
+            opt4.style.background="red"
+        }
+    });
+    opt2.addEventListener("click",function(){
+        if(opt2.innerHTML==ans[i]){
+            next.style.visibility="visible";
+            opt2.style.background="green";
+            opt1.style.background="red"
+            opt3.style.background="red"
+            opt4.style.background="red"
+        }
+    });
+    opt3.addEventListener("click",function(){
+        if(opt3.innerHTML==ans[i]){
+            next.style.visibility="visible";
+            opt3.style.background="green";
+            opt1.style.background="red"
+            opt2.style.background="red"
+            opt4.style.background="red"
+        }
+    });
+    opt4.addEventListener("click",function(){
+        if(opt4.innerHTML==ans[i]){
+            next.style.visibility="visible";
+            opt4.style.background="green";
+            opt1.style.background="red"
+            opt2.style.background="red"
+            opt3.style.background="red"
+        }
+    });
 }
